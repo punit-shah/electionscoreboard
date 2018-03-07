6fd3a7c8cc53c27956e20162af755b4a100ea7bb
Punit Shah <punit.shah@bbc.co.uk>
2018-03-07 18:17:12 +0000

    requests file every 3 seconds

--- src/main/java/uk/co/bbc/datasystems/electionscoreboard/Main.java
+++ src/main/java/uk/co/bbc/datasystems/electionscoreboard/Main.java
@@ -3,33 +3,41 @@ package uk.co.bbc.datasystems.electionscoreboard;
 import uk.co.bbc.datasystems.electionscoreboard.xml.*;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         XmlFileIterator xmlFileIterator = new XmlFileIterator();
 
-        for (int i = 0; i < 5; i++) {
-            XmlFile xmlFile = xmlFileIterator.requestNextFile();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                XmlFile xmlFile = xmlFileIterator.requestNextFile();
+                System.out.println(xmlFile.getFilename());
 
-            System.out.println(xmlFile.getFilename());
+                ConstituencyResults constituencyResults = xmlFile.toObject();
+                printConstituencyResults(constituencyResults);
+            }
+        };
 
-            ConstituencyResults constituencyResults = xmlFile.toObject();
-            List<Constituency> constituencyResultsList = constituencyResults.getConstituencies();
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(runnable, 0, 3, TimeUnit.SECONDS);
+    }
 
-            constituencyResultsList.forEach(constituency -> {
-                System.out.println("seqNo: " + constituency.getSeqNo());
-                System.out.println("constituencyId: " + constituency.getConstituencyId());
-                System.out.println(constituency.getConstituencyName());
+    private static void printConstituencyResults(ConstituencyResults constituencyResults) {
+        List<Constituency> constituencyResultsList = constituencyResults.getConstituencies();
 
-                List<Party> parties = constituency.getResults();
-                parties.forEach(party -> {
-                    System.out.println(party.getPartyCode());
-                    System.out.println("votes: " + party.getVotes());
-                    System.out.println("share: " + party.getShare());
-                });
+        constituencyResultsList.forEach(constituency -> {
+            System.out.println(constituency.getConstituencyName());
+
+            List<Party> parties = constituency.getResults();
+            parties.forEach(party -> {
+                System.out.println(party.getPartyCode() + "\t" + party.getVotes() + "\t" + party.getShare());
             });
+        });
 
-            System.out.println();
-        }
+        System.out.println();
     }
 }
