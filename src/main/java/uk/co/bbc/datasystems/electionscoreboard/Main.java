fb69bfe64f1fe52769658f16bec12d27f7bb4c10
Punit Shah <punit.shah@bbc.co.uk>
2018-03-12 18:19:16 +0000

    removed old printConstituencyResults method

--- src/main/java/uk/co/bbc/datasystems/electionscoreboard/Main.java
+++ src/main/java/uk/co/bbc/datasystems/electionscoreboard/Main.java
@@ -1,9 +1,6 @@
 package uk.co.bbc.datasystems.electionscoreboard;
 
 import uk.co.bbc.datasystems.electionscoreboard.view.ScoreboardView;
-import uk.co.bbc.datasystems.electionscoreboard.xml.*;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,22 +9,5 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    private static void printConstituencyResults(ConstituencyResults constituencyResults) {
-        List<Constituency> constituencyResultsList = constituencyResults.getConstituencies();
-
-        constituencyResultsList.forEach(constituency -> {
-            System.out.println(constituency.getConstituencyName());
-
-            System.out.println(String.format("%s\t%6s\t%s", "Party", "Votes", "Share"));
-
-            List<ConstituencyParty> parties = constituency.getParties();
-            parties.sort((party1, party2) -> party2.getVotes() - party1.getVotes());
-            parties.forEach(party -> System.out.println(
-                String.format("%-5s\t%6d\t%5.1f", party.getPartyCode(), party.getVotes(), party.getShare()))
-            );
-            System.out.println("--------------------------------");
-        });
     }
 }
