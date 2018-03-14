52ba2b3b77d565c1b35c78da3f91dfe88cd1bb56
Punit Shah <punit.shah@bbc.co.uk>
2018-03-14 17:55:00 +0000

    scoreboard get endpoint
    returns scoreboard output as html <pre /> element

--- rest-application/src/main/java/punitshah/electionscoreboard/restapplication/ScoreboardRestController.java
+++ rest-application/src/main/java/punitshah/electionscoreboard/restapplication/ScoreboardRestController.java
@@ -1,10 +1,16 @@
 package punitshah.electionscoreboard.restapplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import punitshah.electionscoreboard.scoreboard.controller.ScoreboardController;
 import punitshah.electionscoreboard.scoreboard.model.ConstituencyResults;
+import punitshah.electionscoreboard.scoreboard.model.Party;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class ScoreboardRestController {
@@ -17,5 +23,53 @@ public class ScoreboardRestController {
     @PostMapping("/constituency-results")
     public void postConstituencyResults(@RequestBody ConstituencyResults constituencyResults) {
         scoreboardController.updateParties(constituencyResults);
+    }
+
+    @GetMapping("/scoreboard")
+    public String getScoreboard() {
+        List<Party> topThreeParties = scoreboardController.getTopThreeParties();
+        List<Party> partiesNotInTopThree = scoreboardController.getPartiesNotInTopThree();
+        Map<String, Double> share = scoreboardController.calculateShare();
+
+        String scoreboard = "<pre>";
+        scoreboard += String.format("%s\t%5s\t%s<br>", "Party", "Seats", "Share");
+        scoreboard += topThreePartiesToString(topThreeParties, share) + "<br>";
+        scoreboard += partiesNotInTopThreeCombinedToString(partiesNotInTopThree, share) + "<br>";
+        scoreboard += "</pre>";
+
+        return scoreboard;
+    }
+
+    private String topThreePartiesToString(List<Party> topThreeParties, Map<String, Double> share) {
+        return String.join(
+                "<br>",
+                topThreeParties.stream()
+                        .map(party -> {
+                            String partyCode = party.getPartyCode();
+                            int seats = party.getSeats();
+
+                            String partyResultString = String.format(
+                                    "%-6s\t%5d\t%5.1f", partyCode, seats, share.get(partyCode)
+                            );
+
+                            return seats >= 326 ? partyResultString + " - Majority won" : partyResultString;
+                        })
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private String partiesNotInTopThreeCombinedToString(List<Party> partiesNotInTopThree, Map<String, Double> share) {
+        double partiesNotInTopThreeCombinedShare = 0.0;
+
+        for (Party party: partiesNotInTopThree) {
+            partiesNotInTopThreeCombinedShare += share.get(party.getPartyCode());
+        }
+
+        return String.format(
+                "%-6s\t%5d\t%5.1f",
+                "Others",
+                scoreboardController.getTotalSeats(partiesNotInTopThree),
+                partiesNotInTopThreeCombinedShare
+        );
     }
 }
