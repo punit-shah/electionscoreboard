5f63d4fe294e2cb97e70f234baca39c6c5888fa4
Punit Shah <punit.shah@bbc.co.uk>
2018-03-29 18:16:20 +0100

    updated rest controller to use new controller api

--- rest-application/src/main/java/punitshah/electionscoreboard/restapplication/ScoreboardRestController.java
+++ rest-application/src/main/java/punitshah/electionscoreboard/restapplication/ScoreboardRestController.java
@@ -10,6 +10,7 @@ import punitshah.electionscoreboard.scoreboard.model.Constituency;
 import punitshah.electionscoreboard.scoreboard.model.ConstituencyResults;
 import punitshah.electionscoreboard.scoreboard.model.Party;
 
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,22 +25,37 @@ public class ScoreboardRestController {
 
     @PostMapping("/constituency-results")
     public ResponseEntity postConstituencyResults(@Validated @RequestBody ConstituencyResults constituencyResults) {
-        if (scoreboardController.updateConstituencies(constituencyResults)) {
-            scoreboardController.updateParties(constituencyResults);
-            return new ResponseEntity(HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity(HttpStatus.CONFLICT);
+        try {
+            if (scoreboardController.updateConstituencies(constituencyResults)) {
+                return new ResponseEntity(HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity(HttpStatus.CONFLICT);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
+
     }
 
     @GetMapping("/parties")
     public List<Party> getParties() {
-        return scoreboardController.getSortedPartyList();
+        try {
+            return scoreboardController.getSortedPartyList();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @GetMapping("/parties/{partyCode}")
     public ResponseEntity<Party> getParty(@PathVariable(name = "partyCode") String partyCode) {
-        Party party = scoreboardController.getPartyMap().get(partyCode);
+        Party party = null;
+        try {
+            party = scoreboardController.getPartyMap().get(partyCode);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return party == null ?
                 new ResponseEntity<>(HttpStatus.NOT_FOUND) :
@@ -48,12 +64,22 @@ public class ScoreboardRestController {
 
     @GetMapping("/constituencies")
     public List<Constituency> getConstituencies() {
-        return scoreboardController.getSortedConstituencyList();
+        try {
+            return scoreboardController.getConstituencyList();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @GetMapping("/constituencies/{constituencyId}")
     public ResponseEntity<Constituency> getConstituency(@PathVariable(name = "constituencyId") int constituencyId) {
-        Constituency constituency = scoreboardController.getConstituency(constituencyId);
+        Constituency constituency = null;
+        try {
+            constituency = scoreboardController.getConstituency(constituencyId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return constituency == null ?
                 new ResponseEntity<>(HttpStatus.NOT_FOUND) :
@@ -62,36 +88,44 @@ public class ScoreboardRestController {
 
     @GetMapping("/scoreboard")
     public Map<String, Map<String, Object>> getScoreboard() {
-        List<Party> topThreeParties = scoreboardController.getTopThreeParties();
-        List<Party> partiesNotInTopThree = scoreboardController.getPartiesNotInTopThree();
-        Map<String, Double> share = scoreboardController.calculateShare();
+         try {
+            List<Party> topThreeParties = scoreboardController.getTopThreeParties();
+            List<Party> partiesNotInTopThree = scoreboardController.getPartiesNotInTopThree();
+            Map<String, Double> share = scoreboardController.calculateShare();
 
-        Map<String, Map<String, Object>> scoreboard = new LinkedHashMap<>();
+            Map<String, Map<String, Object>> scoreboard = new LinkedHashMap<>();
 
-        topThreeParties.forEach(party -> {
-            Map<String, Object> scoreboardEntry = new LinkedHashMap<>();
+            topThreeParties.forEach(party -> {
+                Map<String, Object> scoreboardEntry = new LinkedHashMap<>();
 
-            String partyCode = party.getPartyCode();
+                String partyCode = party.getPartyCode();
 
-            scoreboardEntry.put("seats", party.getSeats());
-            scoreboardEntry.put("share", share.get(partyCode));
-            scoreboardEntry.put("majorityWon", party.isMajorityWon());
+                scoreboardEntry.put("seats", party.getSeats());
+                scoreboardEntry.put("share", share.get(partyCode));
+                scoreboardEntry.put("majorityWon", party.isMajorityWon());
 
-            scoreboard.put(partyCode, scoreboardEntry);
-        });
+                scoreboard.put(partyCode, scoreboardEntry);
+            });
 
-        Map<String, Object> partiesNotInTopThreeCombinedScoreboardEntry = new LinkedHashMap<>();
-        double partiesNotInTopThreeCombinedShare = 0.0;
+            Map<String, Object> partiesNotInTopThreeCombinedScoreboardEntry = new LinkedHashMap<>();
+            double partiesNotInTopThreeCombinedShare = 0.0;
 
-        for (Party party: partiesNotInTopThree) {
-            partiesNotInTopThreeCombinedShare += share.get(party.getPartyCode());
+            for (Party party: partiesNotInTopThree) {
+                partiesNotInTopThreeCombinedShare += share.get(party.getPartyCode());
+            }
+
+            partiesNotInTopThreeCombinedScoreboardEntry.put(
+                    "seats",
+                    scoreboardController.getTotalSeats(partiesNotInTopThree)
+            );
+            partiesNotInTopThreeCombinedScoreboardEntry.put("share", partiesNotInTopThreeCombinedShare);
+
+            scoreboard.put("Others", partiesNotInTopThreeCombinedScoreboardEntry);
+
+            return scoreboard;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
-
-        partiesNotInTopThreeCombinedScoreboardEntry.put("seats", scoreboardController.getTotalSeats(partiesNotInTopThree));
-        partiesNotInTopThreeCombinedScoreboardEntry.put("share", partiesNotInTopThreeCombinedShare);
-
-        scoreboard.put("Others", partiesNotInTopThreeCombinedScoreboardEntry);
-
-        return scoreboard;
     }
 }
