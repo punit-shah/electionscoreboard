97df3542fbac1eb07863c1b5b0b677c3266ef529
Punit Shah <punit.shah@bbc.co.uk>
2018-03-29 18:09:21 +0100

    updated controller to use new model api

--- scoreboard/src/main/java/punitshah/electionscoreboard/scoreboard/controller/ScoreboardController.java
+++ scoreboard/src/main/java/punitshah/electionscoreboard/scoreboard/controller/ScoreboardController.java
@@ -2,6 +2,7 @@ package punitshah.electionscoreboard.scoreboard.controller;
 
 import punitshah.electionscoreboard.scoreboard.model.*;
 
+import java.sql.SQLException;
 import java.util.*;
 
 public class ScoreboardController {
@@ -11,19 +12,15 @@ public class ScoreboardController {
         scoreboardModel = new ScoreboardModel();
     }
 
-    public List<Constituency> getConstituencyList() {
+    public List<Constituency> getConstituencyList() throws SQLException {
         return scoreboardModel.getConstituencyList();
     }
 
-    public List<Constituency> getSortedConstituencyList() {
-        return scoreboardModel.getSortedConstituencyList();
-    }
-
-    public Constituency getConstituency(int constituencyId) {
+    public Constituency getConstituency(int constituencyId) throws SQLException {
         return scoreboardModel.getConstituencyById(constituencyId);
     }
 
-    public boolean updateConstituencies(ConstituencyResults constituencyResults) {
+    public boolean updateConstituencies(ConstituencyResults constituencyResults) throws SQLException {
         Constituency constituency = constituencyResults.getConstituencies().get(0);
 
         if (!scoreboardModel.checkConstituencyIsPresent(constituency.getConstituencyId())) {
@@ -34,29 +31,29 @@ public class ScoreboardController {
         }
     }
 
-    public Map<String, Party> getPartyMap() {
+    public Map<String, Party> getPartyMap() throws SQLException {
         return scoreboardModel.getParties();
     }
 
-    public List<Party> getPartyList() {
+    public List<Party> getPartyList() throws SQLException {
         return scoreboardModel.getPartyList();
     }
 
-    public List<Party> getSortedPartyList() {
+    public List<Party> getSortedPartyList() throws SQLException {
         return scoreboardModel.getSortedPartyList();
     }
 
-    public List<Party> getTopThreeParties() {
+    public List<Party> getTopThreeParties() throws SQLException {
         List<Party> partyList = scoreboardModel.getSortedPartyList();
         return new ArrayList<>(partyList.subList(0, 3));
     }
 
-    public List<Party> getPartiesNotInTopThree() {
+    public List<Party> getPartiesNotInTopThree() throws SQLException {
         List<Party> partyList = scoreboardModel.getSortedPartyList();
         return new ArrayList<>(partyList.subList(3, partyList.size()));
     }
 
-    public Map<String, Double> calculateShare() {
+    public Map<String, Double> calculateShare() throws SQLException {
         Map<String, Double> share = new HashMap<>();
         List<Party> partyList = scoreboardModel.getPartyList();
         double totalVotes = getTotalVotes(partyList);
@@ -68,41 +65,11 @@ public class ScoreboardController {
         return share;
     }
 
-    public void updateParties(ConstituencyResults constituencyResults) {
-        Constituency constituency = constituencyResults.getConstituencies().get(0);
-        List<ConstituencyParty> constituencyParties = constituency.getParties();
-
-        addParties(constituencyParties);
-        updateVotesForEachParty(constituencyParties);
-        updateSeatsForWinningParty(constituencyParties);
-    }
-
     public int getTotalVotes(List<Party> parties) {
         return parties.stream().mapToInt(Party::getVotes).sum();
     }
 
     public int getTotalSeats(List<Party> parties) {
         return parties.stream().mapToInt(Party::getSeats).sum();
-    }
-
-    private void addParties(List<ConstituencyParty> constituencyParties) {
-        constituencyParties.stream()
-                .map(ConstituencyParty::getPartyCode)
-                .forEach(partyCode -> scoreboardModel.addParty(partyCode));
-    }
-
-    private void updateVotesForEachParty(List<ConstituencyParty> constituencyParties) {
-        constituencyParties.forEach(constituencyParty ->
-                scoreboardModel.updateVotesForParty(constituencyParty.getPartyCode(), constituencyParty.getVotes())
-        );
-    }
-
-    private void updateSeatsForWinningParty(List<ConstituencyParty> constituencyParties) {
-        ConstituencyParty winningParty = getWinningConstituencyParty(constituencyParties);
-        scoreboardModel.incrementSeatsForParty(winningParty.getPartyCode());
-    }
-
-    private ConstituencyParty getWinningConstituencyParty(List<ConstituencyParty> parties) {
-        return parties.stream().max(Comparator.comparingDouble(ConstituencyParty::getShare)).orElse(null);
     }
 }
